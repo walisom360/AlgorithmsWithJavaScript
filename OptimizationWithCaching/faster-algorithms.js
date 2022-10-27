4813f2850036bce2aa1e2fd34554ff47ea2e07e2
walison matheus <walisom360@hotmail.com>
2022-10-26 22:03:14 -0300

    first commit

---
+++ OptimizationWithCaching/faster-algorithms.js
@@ -0,0 +1,20 @@
+const isUnique = (arr) => {
+  let result = true;
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`~~~~~~ OUTER LOOP i === ${i}`);
+
+    for (let j = 0; j < arr.length; j++) {
+      console.log(`~~~~~~ INNER LOOP i === ${j}`);
+
+      if (i !== j && arr[i] === arr[j]) {
+        result = false;
+      }
+    }
+
+    return result;
+  }
+};
+
+console.log(isUnique([1, 2, 3]) === true);
+console.log(isUnique([1, 2, 3]) === false);
