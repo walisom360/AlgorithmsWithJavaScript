b8711fe13cd5dab94a7fce6cf74a8bca3a85fe7a
walison matheus <walisom360@hotmail.com>
2022-10-26 22:10:23 -0300

    add cache

--- OptimizationWithCaching/faster-algorithms.js
+++ OptimizationWithCaching/faster-algorithms.js
@@ -16,4 +16,22 @@ const isUnique = (arr) => {
   }
 };
 
-console.log(isUnique([1, 2, 3]) === true);
+// console.log(isUnique([1, 2, 3]) === true);
+
+const isUniqueWithCache = (arr) => {
+  const breadcrumbs = {};
+  let result = true;
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`~~~~~~ LOOP i === ${i}`);
+    if (breadcrumbs[arr[i]]) {
+      result = false;
+    } else {
+      breadcrumbs[arr[i]] = true;
+    }
+  }
+
+  return result;
+};
+
+console.log(isUniqueWithCache([1, 2, 3]) === true);
