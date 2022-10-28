e6074c9960802750eec29f0809619423c26e4756
walison matheus <walisom360@hotmail.com>
2022-10-27 22:43:29 -0300

    add basic memoizaton exercise

---
+++ OptimizationWithCaching/basic-memoization/exercise.js
@@ -0,0 +1,19 @@
+// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
+// a simple multiplication fn
+const times10 = () => {};
+
+console.log("~~~~~~~~~~TASK 1~~~~~~~~~~~~~~~~");
+console.log("times10 reuturns:", times10(9));
+
+//Task 2: Use an object to cache the result of you times10 function.
+// protip 1: Create a function that checks if the value for n has been calculated
+//  before.
+// protip 2: If the value for n has not been calculated, calculated, calculate and the save
+// the result in the cache object.
+
+const cache = {};
+
+const memoTimes = (n) => {};
+
+console.log("~~~~~~~~TASK 2~~~~~~~~~~");
+console.log("Task 2 calculated value:", memoTimes(9)); // calculated
